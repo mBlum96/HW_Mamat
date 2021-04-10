4bb3edde03002f6182717f9c9fb0eb6a533e415b
mBlumTechnion <m.blum.1996@campus.technion.ac.il>
2021-04-10 22:05:52 +0300

    finished script, added for loop and deletes unecessary files

--- HW_1/scrape_players.sh
+++ HW_1/scrape_players.sh
@@ -4,4 +4,6 @@ cat players | grep -Po "/players/[0-9]+/[a-z A-Z]+[- a-z A-Z]*/overview" > urls.
 cat urls.txt | wc -l
 sed -i 's/\/players/https:\/\/www\.premierleague\.com\/players/' urls.txt
 urls_cat=$(cat urls.txt)
-wget $urls_cat
+for i in $urls_cat; do wget $i; count_midfielder=$(cat overview | grep -Po "Midfielder|midfielder" | wc -w); rm overview; if [ $count_midfielder -gt 0 ]; then echo "$i, Midfielder, $count_midfielder" >> results.csv; fi; done
+rm urls.txt
+rm players
