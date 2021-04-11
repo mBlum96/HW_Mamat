175bfa411fab73ef9d30099020a2de1736d6cc89
mBlumTechnion <m.blum.1996@campus.technion.ac.il>
2021-04-11 16:33:57 +0300

    fixed a bug that made the script add to the file instead of writting it from scratch

--- HW_1/scrape_players.sh
+++ HW_1/scrape_players.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 wget https://www.premierleague.com/players
 cat players | grep -Po "/players/[0-9]+/[a-z A-Z]+[- a-z A-Z]*/overview" > urls.txt
-cat urls.txt | wc -l
+total_players=$(cat urls.txt | wc -l)
+echo "Total Players: $total_players" > results.csv
 sed -i 's/\/players/https:\/\/www\.premierleague\.com\/players/' urls.txt
 urls_cat=$(cat urls.txt)
 for i in $urls_cat; do wget $i; count_midfielder=$(cat overview | grep -Po "Midfielder|midfielder" | wc -w); rm overview; if [ $count_midfielder -gt 0 ]; then echo "$i, Midfielder, $count_midfielder" >> results.csv; fi; done
