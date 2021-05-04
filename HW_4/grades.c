0d61ca02fc8f973db0200bfa902033bc4e2c998e
mBlumTechnion <m.blum.1996@campus.technion.ac.il>
2021-05-04 16:15:52 +0300

    Second commit, written clone and destroy functions

--- HW_4/grades.c
+++ HW_4/grades.c
@@ -7,26 +7,90 @@
 /**
 Put here comments in doxygen, if needed checkout the hello-world file
  */
-int elem_clone (void *element, void **output){
-
-}
-void elem_destroy (void *element){
-
-}
 
 struct grades{
-	struct list students;
+	struct list *students;
 };
+
+struct student{
+	struct list *courses;
+	int id;
+	char *name;
+};
+
+struct course{
+	char *course_name;
+	int course_grade;
+};
+int course_clone (void *element, void **output){
+	struct course *new_element = (struct course*) element;
+	struct course *copy_element =
+			(struct course*)malloc(sizeof(struct course));
+	if(!copy_element){
+			return 1;
+	}
+	copy_element->course_name=
+			(char*)malloc(sizeof(char)*(strlen(new_element->course_name)+1));
+	if((copy_element->course_name)==NULL){
+		free(copy_element);
+		return 1;
+	}
+	strcpy(copy_element->course_name,new_element->course_name);
+	copy_element->course_grade=new_element->course_grade;
+	*output = (void*) copy_element;
+}
+
+void course_destroy(void *element){
+	struct course *destroy_element = (struct course*) element;
+	free(destroy_element->course_name);
+	free(destroy_element);
+}
+
+int student_clone (void *element, void **output){
+	struct student *new_element = (struct student*) element;
+	struct student *copy_element =
+			(struct student*)malloc(sizeof(struct student));
+	if(!copy_element){
+		return 1;
+	}
+	copy_element->name =
+			(char*)malloc(sizeof(char)*(strlen(new_element->name)+1));
+	if((copy_element->name)==NULL){
+		free(copy_element);
+		return 1;
+	}
+	strcpy(copy_element->name,new_element->name);
+	copy_element->id = new_element->id;
+	copy_element->courses = list_init(course_clone, course_destroy);
+	if((copy_element->courses)==NULL){
+		free(copy_element->name);
+		free(copy_element);
+		return 1;
+	}
+	*output = (void*) copy_element;
+}
+void student_destroy (void *element){
+	struct student *destroy_element = (struct student*) element;
+	free(destroy_element->name);
+	list_destroy(destroy_element->courses);
+	free(destroy_element);
+}
+
 
 //first function implementation
 struct grades* grades_init(){
-	struct grades *grades;
-	grades = (strict grades*)malloc(sizeof(*grades));
-	if (!grades){
+	struct grades *Grades;
+	Grades = (struct grades*)malloc(sizeof(*Grades));
+	if (!Grades){
 		return NULL;
 	}
-	grades -> studetns = list_init(elem_clone,elem_destroy);
-	return students;
+	Grades->students = list_init(student_clone,student_destroy);
+	if(Grades->students == NULL)
+	{
+		free(Grades);
+		return NULL;
+	}
+	return (Grades);
 }
 
 void grades_destroy(struct grades *grades){
