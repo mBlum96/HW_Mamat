20115568144ffdc45d9a5e0b562c59c66ad6682d
mBlumTechnion <m.blum.1996@campus.technion.ac.il>
2021-05-04 17:28:35 +0300

    Fourth commit, added function find course name

--- HW_4/grades.c
+++ HW_4/grades.c
@@ -4,10 +4,8 @@
 #include <linked-list.h>
 
 
-/**
-Put here comments in doxygen, if needed checkout the hello-world file
- */
 
+/* Defining the needed structs */
 struct grades{
 	struct list *students;
 };
@@ -22,6 +20,50 @@ struct course{
 	char *course_name;
 	int course_grade;
 };
+
+
+
+
+struct student* search_students_id (struct grades *grades, int id){
+	if (!grades){
+		return NULL;
+	}
+	struct iterator it = list_begin (grades->students);
+	struct student *list_p;
+
+	while (it!=NULL){
+		list_p = list_get(it);
+		if (list_p->id == id){
+			return list_p;
+		}
+		it =list_next(it);
+	}
+	return NULL;
+}
+
+struct course* search_course_name(struct grades *grades, char* course_name){
+	if(!grades){
+		return NULL;
+	}
+	struct iterator it = list_begin(grades->students);
+	struct course *course_p;
+
+	while(it!=NULL){
+		course_p = list_get(it);
+		if (strcmp(course_p->course_name,course_name)==0){
+			return course_p;
+		}
+		it = list_next(it);
+	}
+	return NULL;
+}
+
+/**
+ * @brief Used to clone course to the heap
+ * @param element A pointer to the course we want to be cloned into the heap
+ * @param output A pointer to the pointer of the course on the heap
+ * @returns Value of 1 if there has been a problem and 0 if successful
+ */
 int course_clone (void *element, void **output){
 	struct course *new_element = (struct course*) element;
 	struct course *copy_element =
@@ -40,12 +82,22 @@ int course_clone (void *element, void **output){
 	*output = (void*) copy_element;
 }
 
+/**
+ * @brief De-allocates the heap memory taken by the course data struct
+ * @param element A pointer to the course we want to destroy
+ */
 void course_destroy(void *element){
 	struct course *destroy_element = (struct course*) element;
 	free(destroy_element->course_name);
 	free(destroy_element);
 }
 
+/**
+ * @brief Used to clone student to the heap
+ * @param element A pointer to the student we want to be cloned into the heap
+ * @param output A pointer to the pointer of the student on the heap
+ * @returns Value of 1 if there has been a problem and 0 if successful
+ */
 int student_clone (void *element, void **output){
 	struct student *new_element = (struct student*) element;
 	struct student *copy_element =
@@ -69,6 +121,11 @@ int student_clone (void *element, void **output){
 	}
 	*output = (void*) copy_element;
 }
+
+/**
+ * @brief De-allocates the heap memory taken by the student data struct
+ * @param element A pointer to the student we want to destroy
+ */
 void student_destroy (void *element){
 	struct student *destroy_element = (struct student*) element;
 	free(destroy_element->name);
@@ -77,33 +134,56 @@ void student_destroy (void *element){
 }
 
 
-//first function implementation
+/**
+ * @brief Used to initiate the grades structure on the heap
+ * @returns A pointer to the grades structure, NULL in case of error
+ */
 struct grades* grades_init(){
-	struct grades *Grades;
-	Grades = (struct grades*)malloc(sizeof(*Grades));
-	if (!Grades){
+	struct grades *grades;
+	grades = (struct grades*)malloc(sizeof(struct grades));
+	if (!grades){
 		return NULL;
 	}
-	Grades->students = list_init(student_clone,student_destroy);
-	if(Grades->students == NULL)
+	grades->students = list_init(student_clone,student_destroy);
+	if(grades->students == NULL)
 	{
-		free(Grades);
+		free(grades);
 		return NULL;
 	}
-	return (Grades);
+	return (grades);
 }
 
+/**
+ * @brief De-allocates all memory from the heap
+ * @param grades Pointer to the data we want to de-allocate
+ */
 void grades_destroy(struct grades *grades){
 	if (!grades){
 		return;
 	}
-	while(students){
-		list_destroy(students);
-	}
-	free(students);
+	list_destroy(grades->students);
+	free(grades);
 }
 
 int grades_add_student(struct grades *grades, const char *name, int id){
 
 
 }
+
+int grades_add_grade(struct grades *grades,
+                     const char *name,
+                     int id,
+                     int grade){
+	struct student *student_location = search_student_id(grades,id);
+	if(student_location!=0){
+
+	}
+
+
+}
+
+
+
+
+
+
