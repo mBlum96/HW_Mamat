e880ae02ec191e09bb4e21ef091282d28f966993
Yossi Meshulam <yossi1146@gmail.com>
2021-06-08 22:04:09 +0300

    final files

--- HW_6/string.cpp
+++ HW_6/string.cpp
@@ -4,6 +4,7 @@
 #include "string.h"
 #include <cstdlib>
 
+#define EMPTY_LOC -1
 #define SPACE ' '
 
 using namespace std;
@@ -30,15 +31,10 @@ String::String(const String &str){
  * @brief Initiates a string from char array
  */
 String::String(const char *str){
-//	if(str == NULL || strlen(str) == 0){
-//		data = nullptr;
-//		length = 0;
-//	}
-//	else{
-		length = strlen(str);
-		data = new char[length+1]();
-		strcpy(data,str);
-//	}
+
+    length = strlen(str);
+    data = new char[length+1]();
+    strcpy(data,str);
 }
 
 
@@ -82,7 +78,7 @@ bool String::equals(const String &rhs) const{
 bool String::equals(const char *rhs) const{
     return !strcmp(rhs, this->data);
 }
-
+//
 /**
  * @brief Splits this to several sub-strings according to delimiters.
  * Allocates memory for output that the user must delete (using delete[]).
@@ -90,125 +86,64 @@ bool String::equals(const char *rhs) const{
  * @note If "output" is set to NULL, do not allocated memory, only
  * compute "size".
  */
-
-
-//void String::split(const char *delimiters, String **output, size_t *size) const{
-    // delete copy
-//    char *throwaway_str;
- //   int counter = 0;
- //   throwaway_str = strtok(this->data,delimiters);
-  //  while (throwaway_str!=nullptr){
-  //      counter ++;
-  //      throwaway_str = strtok(NULL,delimiters);
- //   }
-  //  *output = new String[counter]; //change Char to String
-  //  throwaway_str = strtok(this->data,delimiters);
-  //  while (throwaway_str!=nullptr){
-  //      **output = String(throwaway_str); //change to copy constructor
-  //      output++;               //are we sure?
-  //      throwaway_str = strtok(nullptr,delimiters);
-  //  }
- //   *size = counter; //change
-//}
-//---------------------------------------------------------------------------------------------------
-
 void String::split(const char *delimiters, String **output, size_t *size) const{
-	if (delimiters == NULL || size == NULL){
-		return;
-	}
-	size_t num_of_strings = 1;
-	int left = 0;
-	int index = 0;
-	char deli[strlen(delimiters)+1];
-	strcpy(deli,delimiters);
-
-	if(this->data == NULL){
-		*size = 0;
-		*output = NULL;
-		return;
-	}
-	char tmp[length + 1];
-	strcpy(tmp,data);
-
-	/* count numbers of sub strings*/
-	 for(int j = 0; '\0' != tmp[j]; j++){
-	 	for(int i = 0; '\0' != deli[i]; i++){
-	 		if (deli[i] == tmp[j]){
-	 			num_of_strings +=1;
-	 		}
-	 	}
-	 }
-
-	 /* IF OUTPUT IS null go back with num of sub Strings*/
-	 if (output == NULL){
-		*size = num_of_strings;
-		return;
-	}	
-
-
-	if (num_of_strings == 1){
-		*output = new String[num_of_strings];
-		(*output)[index] = String(tmp);
-		*size = num_of_strings;
-		return;
-	}
-
-	
-	*output = new String[num_of_strings];
-	//String string_array[num_of_strings];
-	
-	*size = num_of_strings;
-	if (output == NULL){
-		*size = num_of_strings;
-		return;
-	}
-
-
-	int startSplitFrom = 0;
-	/* special case if the input STARTS
-	 with 2 delimeters one after one*/
-	for(int i = 0; '\0' != deli[i]; i++){
-		if(tmp[0] == deli[i]){
-			(*output)[index] =String();
-			index = 1;
-	 		startSplitFrom = 1;
-		}
-	}
-
-	 for(int j = startSplitFrom; '\0' != tmp[j]; j++){
-	 	for(int i = 0; '\0' != deli[i]; i++){
-	 		if (deli[i] == tmp[j]){
-	 			if (left != j){
-	 			tmp[j] = '\0';
-	 			(*output)[index] =String(&tmp[left]);
-	 		}
-	 		else{
-	 			(*output)[index] =NULL;
-	 			 }
-	 			left = j+1;
-	 			index+=1; 	
-	 		}
-	 	}
-	}
-	/* special case if the input ENDS
-	 with 2 delimeters one after one*/
-	for(int i = 0; '\0' != deli[i]; i++){
-		if(tmp[length-1] == deli[i]){
-			(*output)[index] =NULL;
-		}
-		else{
-			(*output)[index] =String(&tmp[left]);
-		}
-	}
-
-	return;
+    if (delimiters == NULL || size == 0){
+        return;
+    }
+    size_t sub_str_arr_len = 1;
+    int index = 0;
+    char target_arr[strlen(delimiters)+1];
+    strcpy(target_arr,delimiters);
+    if(this->data == NULL){
+        *size = 0;
+        *output = NULL;
+        return;
+    }
+    char throwaway_str[length + 1];
+    strcpy(throwaway_str,data);
+    for(int j = 0; throwaway_str[j]!='\0'; j++){
+        for(int i = 0; target_arr[i]!='\0'; i++){
+            if (target_arr[i] == throwaway_str[j]){
+                sub_str_arr_len +=1;
+            }
+        }
+    }
+    if (output == NULL){
+        *size = sub_str_arr_len;
+        return;
+    }
+    *output = new String[sub_str_arr_len];
+    *size = sub_str_arr_len;
+    int left_idx = 0;
+    for(int i = 0; throwaway_str[i]!='\0'; i++){
+        for(int j = 0; target_arr[j]!='\0'; j++){
+            if (target_arr[j] == throwaway_str[i]){
+                if (left_idx != i){
+                    throwaway_str[i] = '\0';
+                    (*output)[index] =String(&throwaway_str[left_idx]);
+                }
+                else{
+                    (*output)[index] =NULL;
+                }
+                left_idx = i+1;
+                index++;
+            }
+        }
+    }
+    for(int i = 0; target_arr[i]!='\0'; i++){
+        if(throwaway_str[length-1] == target_arr[i]){
+            (*output)[index] =NULL;
+        }
+        else{
+            (*output)[index] =String(&throwaway_str[left_idx]);
+        }
+    }
+    return;
 }
-//-----------------------------------------------------------------------------------------------------
 /**
  * @brief Try to convert this to an integer. Returns 0 on error.
  */
 int String::to_integer() const{
-    //int value = 0;  // comp
     return atoi(this->data);
 }
 
@@ -217,47 +152,26 @@ int String::to_integer() const{
  * Does not change this.
  */
 String String::trim() const{
-    //char *copied_string = new char[length+1];
     char copied_string[length+1];
     int leading_index = 0;
-    //int counter = 0; // comp
     int trailing_index = (int)length - 1;
     strcpy(copied_string,data);
-
     while (copied_string[leading_index] == SPACE) {
         leading_index++;
     }
     while (copied_string[trailing_index] == SPACE) {
         trailing_index--;
     }
-
     int len_after_trim = trailing_index-leading_index+1;
-
     char* clean_string = new char[len_after_trim+1]();
     int i = 0;
-
-    //need to make sure
-
     while (leading_index <= trailing_index){
         clean_string[i] = data[leading_index];
         leading_index++;
         i++;
     }
-    
     clean_string[len_after_trim] = '\0';
-    
     String cleaned = String(clean_string);
     delete[](clean_string);
-    //delete[](copied_string);
-
-    //return String(clean_string); //the fun return String
     return cleaned;
 }
-
-//String *a = new String();
-//String *b = new String(a);
-//char str[5] = "abcd";
-//String *c = new String(str);
-//~String();
-
-
